1d44c70c8b90cb840c512091e21e716ee5f5f967
ryu0947 <dragonogawaz@gmail.com>
2020-11-21 21:56:43 +0900

    メソッドチェーンでコードを短縮

--- js/header.js
+++ js/header.js
@@ -1,6 +1,5 @@
 $(function () {
     const headerClone = $("#js-header").contents().clone();
     const headerCloneContainer = $("<div class= 'header-clone fixed'></div>");
-    headerCloneContainer.append(headerClone);
-    headerCloneContainer.appendTo("body");
+    headerCloneContainer.append(headerClone).appendTo("body");
 });
