9cc30d51d79ba26d10def388608eedbec8b63501
ryu <dragonogawaz@gmail.com>
2021-03-09 08:35:10 +0900

    コメント削除

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -14,20 +14,3 @@ $(function(){
     })
   );
 })
-
-// jQuery(function ($) {
-//   const target = $(".js-trigger");
-//   $(window).scroll(
-//     $.throttle(250, function () {
-//       $(target).each(function () {
-//         const scroll = $(window).scrollTop();
-//         const winHeight = $(window).height();
-//         const targetHeight = $(this).offset().top;
-
-//         if (scroll > targetHeight - winHeight / 1.5) {
-//           $(this).addClass("show");
-//         }
-//       });
-//     })
-//   );
-// });
