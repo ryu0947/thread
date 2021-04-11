7003e891ba395aab97efde90e894e55233573f09
ryu <dragonogawaz@gmail.com>
2021-04-12 07:45:57 +0900

    ヘッダーのアニメーション変更

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 $(function () {
     $(window).on("scroll", function() {
-      if($(this).scrollTop() > 700) {
+      if($(this).scrollTop() > 500) {
         $("#js-header").addClass("fixed");
       } else {
         $("#js-header").removeClass("fixed");
