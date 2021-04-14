eeb6a529e7bc886c04a92987c14dca3f1de934b5
ryu <dragonogawaz@gmail.com>
2021-04-15 08:03:12 +0900

    スムーススクロールの実装

--- js/smooth.js
+++ js/smooth.js
@@ -1,2 +1,15 @@
-const anchors = $("header__li a");
-console.log(anchors);
+let target;
+
+$("a[href^='#']").click(function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
+  if (href === "#") {
+    target = $("html");
+  } else {
+    target = $(href);
+  }
+  const position = target.offset().top;
+  const getHeight = $("#js-header").outerHeight();
+  $("body,html").animate({ scrollTop: position - getHeight }, 600, "swing");
+  return false;
+});
