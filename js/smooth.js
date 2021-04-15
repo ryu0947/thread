a5f106b8bd5f54983dbc1bf83f74a18d8f30081c
ryu <dragonogawaz@gmail.com>
2021-04-16 07:04:01 +0900

    backgroundの表記の統一とエラーの修正

--- js/smooth.js
+++ js/smooth.js
@@ -1,7 +1,9 @@
 $(function () {
-  let target;
+  let target = 0;
   $("a[href^='#']").click(function (e) {
     e.preventDefault();
+    $(".js-menu-btn").removeClass("active");
+    $("#js-menu").removeClass("show");
     const href = $(this).attr("href");
     if (href === "#") {
       target = $("");
