90593454ede9e1972e9228f9945f61d371f6c95d
ryu <dragonogawaz@gmail.com>
2021-04-15 22:22:15 +0900

    readyイベント追加

--- js/smooth.js
+++ js/smooth.js
@@ -1,15 +1,16 @@
-let target;
-
-$("a[href^='#']").click(function (e) {
-  e.preventDefault();
-  const href = $(this).attr("href");
-  if (href === "#") {
-    target = $("html");
-  } else {
-    target = $(href);
-  }
-  const getHeight = $("#js-header").outerHeight();
-  const position = target.offset().top - getHeight;
-  $("body,html").animate({ scrollTop: position }, 600, "swing");
-  return false;
+$(function () {
+  let target;
+  $("a[href^='#']").click(function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    if (href === "#") {
+      target = $("html");
+    } else {
+      target = $(href);
+    }
+    const getHeight = $("#js-header").outerHeight();
+    const position = target.offset().top - getHeight;
+    $("body,html").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
 });
